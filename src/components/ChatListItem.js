f2d7054f8a8ce8d7225ee787b55e628a3b66110a
julio <julioakzzo@hotmail.com>
2021-10-05 16:39:58 -0300

    adicionando aba de conversa

---
+++ src/components/ChatListItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './ChatListItem.css'
+
+export default () => {
+    return (
+        <div className="chatListItem">
+            <img className="chatListItem--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="foto perfil" />
+
+            <div className="chatListItem--lines">
+                <div className="chatListItem--line">
+                    <div className="chatListItem--name">Mateuzin</div>
+                    <div className="chatListItem--date">20:00</div>
+                </div>
+
+                <div className="chatListItem--line">
+                    <div className="chatListItem--lastMsg">
+                        <p>Opa, tudo bem?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
