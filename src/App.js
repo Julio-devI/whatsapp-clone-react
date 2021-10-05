d7eb58474c23699900c17c9765eaacdfebaf1a83
julio <julioakzzo@hotmail.com>
2021-10-05 15:00:54 -0300

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './App.css'
 
+import DonutLargeIcon from '@material-ui/icons/DonutLarge'
+import ChatIcon from '@material-ui/icons/Chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+
 export default () => {
   return (
     <div className="app-window">
@@ -10,11 +14,21 @@ export default () => {
           <img className="header--avatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="avatar login" />
           <div className="header--buttons">
 
+            <div className="header--btn">
+              <DonutLargeIcon style={{ color: '#919191' }} />
+            </div>
+            <div className="header--btn">
+              <ChatIcon style={{ color: '#919191' }} />
+            </div>
+            <div className="header--btn">
+              <MoreVertIcon style={{ color: '#919191' }} />
+            </div>
+
           </div>
         </header>
 
         <div className="search">
-
+          ...
         </div>
 
         <div className="chatlist">
